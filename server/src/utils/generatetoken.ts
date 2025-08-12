3056580cb3d6a921bcd92149d0fa2ed36599a309
myo thant naing <thant8642@gmail.com>
2025-08-12 22:22:25 +0630

    deployment

--- server/src/utils/generatetoken.ts
+++ server/src/utils/generatetoken.ts
@@ -9,7 +9,7 @@ const generateToken = (res: Response, userId: Types.ObjectId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
 }
